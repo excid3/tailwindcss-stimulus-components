a86e17298b4a6230374179380f3d11f0aa19403e
Julian Rubisch <julian@julianrubisch.at>
2020-10-07 08:32:23 +0200

    Rewrite dropdown controller to support transitions

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,7 +3,7 @@
 
 // This example controller works with specially annotated HTML like:
 
-// <div class="relative" data-controller="dropdown">
+// <div class="relative" data-controller="dropdown" data-dropdown-inactive-class="invisible" data-dropdown-active-class="visible" data-dropdown-entering-class="entering-transition" data-dropdown-enter-timeout="100" data-dropdown-leaving-class="leaving-transition" data-dropdown-leave-timeout="75">
 //  <div data-action="click->dropdown#toggle click@window->dropdown#hide" role="button" class="inline-block select-none">
 //    Open Dropdown
 //  </div>
@@ -21,30 +21,78 @@ export default class extends Controller {
 
   connect() {
     this.toggleClass = this.data.get('class') || 'hidden'
+    this.inactiveClass = this.data.get('inactiveClass') || null
     this.activeClass = this.data.get('activeClass') || null
+    this.enteringClass = this.data.get('enteringClass') || null
+    this.enterTimeout = parseInt(this.data.get('enterTimeout')) || 0
+    this.leavingClass = this.data.get('leavingClass') || null
+    this.leaveTimeout = parseInt(this.data.get('leaveTimeout')) || 0
   }
 
   toggle() {
-    this.menuTarget.classList.toggle(this.toggleClass)
-    this._activeClassList.forEach((klass) =>  this.activeTarget.classList.toggle(klass))
-  }
-
-  hide(event) {
-    if (
-      this.element.contains(event.target) === false &&
-      !this.menuTarget.classList.contains(this.toggleClass)
-    ) {
-      this.menuTarget.classList.add(this.toggleClass)
-      this._activeClassList.forEach((klass) =>  this.activeTarget.classList.remove(klass))
+    if (this.hidden) {
+      this._show()
+    } else {
+      this._hide()
     }
   }
 
+  _show() {
+    this.menuTarget.classList.remove(this.toggleClass)
+    this._enteringClassList.forEach(klass => {
+      this.activeTarget.classList.add(klass)
+    })
+
+    setTimeout(() => {
+      this._activeClassList.forEach(klass => {
+        this.activeTarget.classList.add(klass)
+      })
+      this._inactiveClassList.forEach(klass => this.activeTarget.classList.remove(klass))
+      setTimeout(() => {
+        this._enteringClassList.forEach(klass => this.activeTarget.classList.remove(klass))
+      }, this.enterTimeout)
+    })
+  }
+
+  _hide() {
+    this._inactiveClassList.forEach(klass => this.activeTarget.classList.add(klass))
+    this._activeClassList.forEach(klass => this.activeTarget.classList.remove(klass))
+    this._leavingClassList.forEach(klass => this.activeTarget.classList.add(klass))
+    setTimeout(() => {
+      this.menuTarget.classList.add(this.toggleClass)
+      this._leavingClassList.forEach(klass => this.activeTarget.classList.remove(klass))
+    }, this.leaveTimeout)
+  }
+
+  hide(event) {
+    if (this.element.contains(event.target) === false && !this.hidden) {
+      this._hide()
+    }
+  }
+
+  get hidden() {
+    return this.menuTarget.classList.contains(this.toggleClass)
+  }
+
   get activeTarget() {
-    return this.data.has('activeTarget') ? document.querySelector(this.data.get('activeTarget')) : this.element
+    return this.data.has('activeTarget')
+      ? document.querySelector(this.data.get('activeTarget'))
+      : this.element
   }
 
   get _activeClassList() {
-    return (!this.activeClass ? [] : this.activeClass.split(' '))
+    return !this.activeClass ? [] : this.activeClass.split(' ')
   }
 
+  get _inactiveClassList() {
+    return !this.inactiveClass ? [] : this.inactiveClass.split(' ')
+  }
+
+  get _enteringClassList() {
+    return !this.enteringClass ? [] : this.enteringClass.split(' ')
+  }
+
+  get _leavingClassList() {
+    return !this.leavingClass ? [] : this.leavingClass.split(' ')
+  }
 }
