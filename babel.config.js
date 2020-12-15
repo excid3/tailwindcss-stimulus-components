81fdb7c999fd5372e5a94c3543b69dd8d8cc75d8
William Kennedy <williamkennedyjnr@gmail.com>
2020-12-15 12:18:44 +0000

    added fallback so @babel/plugin-transform-runtime is only set in NODE_ENV=test

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports =
+  process.env.NODE_ENV === 'test'
+  ? {
+    "presets": ["@babel/env"],
+    "plugins": [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-transform-runtime"
+    ]
+  }
+  : {}
