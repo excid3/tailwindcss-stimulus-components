2ea39a516f717228723ad83e05391bf67af996f0
Hasan Ozovali <ozovalihasan@gmail.com>
2021-11-06 01:07:23 +0300

    Update same target attribute in another file

--- src/popover.js
+++ src/popover.js
@@ -8,7 +8,7 @@
 //  Beginning in 2015, Google introduced what is called the
 //    <div class="popover inline-block" data-controller="popover" data-popover-translate-x="0" data-popover-translate-y="-128%" data-action="mouseover->popover#mouseOver mouseout->popover#mouseOut">
 //      <span class="underline">'local snack pack',</span>
-//      <div class="content hidden absolute max-w-xs bg-grey-light rounded p-2" data-target="popover.content">
+//      <div class="content hidden absolute max-w-xs bg-grey-light rounded p-2" data-popover-target="content">
 //        Terrible name - we know. But the biggest name in SEO came up with it.
 //      </div>
 //    </div>
