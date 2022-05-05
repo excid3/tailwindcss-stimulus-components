7494678bd3119a1473ee3be3a7ace2225b75e648
Ahmad khatab <38360603+rockwellll@users.noreply.github.com>
2022-05-05 12:24:35 +0300

    explicitly show or hide or toggle toggleables

--- src/toggle.js
+++ src/toggle.js
@@ -14,25 +14,44 @@ export default class extends Controller {
     }
 
     this.openValue = !this.openValue
+    this.toggleToggleables()
   }
 
   hide(event) {
     event.preventDefault();
 
     this.openValue = false;
+    this.hideToggleables()
   }
 
   show(event) {
     event.preventDefault();
 
     this.openValue = true;
+    this.showToggleables()
   }
 
-  openValueChanged() {
+  toggleToggleables() {
     if (!this.toggleClass) { return }
 
     this.toggleableTargets.forEach(target => {
       target.classList.toggle(this.toggleClass)
+    })
+  }
+  
+  showToggleables() {
+    if (!this.toggleClass) { return }
+    
+    this.toggleableTargets.forEach(target => {
+      target.classList.remove(this.toggleClass)
+    })
+  }
+  
+  hideToggleables() {
+    if (!this.toggleClass) { return }
+    
+    this.toggleableTargets.forEach(target => {
+      target.classList.add(this.toggleClass)
     })
   }
 }
