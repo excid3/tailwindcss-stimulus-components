54268649051c961113ed614438013e70501dc96d
Dmitry Sychev <dmitry.sychev@me.com>
2023-05-17 08:56:43 -0400

    Updating dist maps (#183)
    * Updating dist maps
    
    * update dist to to master, plus new changes

--- index.d.ts
+++ index.d.ts
@@ -1,63 +1,9 @@
-import {Controller} from '@hotwired/stimulus';
-
-export class Alert extends Controller {
-  close(): void;
-  show(): void;
-  hide(): void;
-}
-
-export class Autosave extends Controller {
-  save(): void;
-  success(): void;
-  error(): void;
-  setStatus(message: string): void;
-}
-
-export class ColorPreview extends Controller {
-  update(): void;
-  // `color` is a hex color string
-  set preview(color: string);
-  get color(): string;
-}
-
-export class Dropdown extends Controller {
-  toggle(): void;
-  show(): void;
-  hide(event: Event): void;
-  openValueChanged(): void;
-  get activeTarget(): Element;
-  get enterTimeout(): number[];
-  get leaveTimeout(): number[];
-}
-
-export class Modal extends Controller {
-  open(event: Event): void;
-  close(event: Event): void;
-  lockScroll(): void;
-  unlockScroll(): void;
-  saveScrollPosition(): void;
-  restoreScrollPosition(): void;
-}
-
-export class Popover extends Controller {
-  mouseOver(): void;
-  mouseOut(): void;
-  toggle(): void;
-}
-
-export class Slideover extends Dropdown {}
-
-export class Tabs extends Controller {
-  change(event: Event): void;
-  showTab(): void;
-  get index(): number;
-  set index(value: number);
-  get anchor(): string | null;
-}
-
-export class Toggle extends Controller {
-  toggle(event: Event): void;
-  hide(event: Event): void;
-  show(event: Event): void;
-  openValueChanged(): void;
-}
+export { default as Alert } from "./alert";
+export { default as Autosave } from "./autosave";
+export { default as Dropdown } from "./dropdown";
+export { default as Modal } from "./modal";
+export { default as Tabs } from "./tabs";
+export { default as Toggle } from "./toggle";
+export { default as Popover } from "./popover";
+export { default as Slideover } from "./slideover";
+export { default as ColorPreview } from "./color_preview";
