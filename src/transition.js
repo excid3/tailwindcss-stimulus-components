e43a384196562fa34a18edbbf1c47d643f0e2d31
Chris Oliver <excid3@gmail.com>
2023-08-01 15:45:44 -0500

    Remove last transition before starting a new one

--- src/transition.js
+++ src/transition.js
@@ -1,6 +1,8 @@
 // A fork of el-transition
 // https://github.com/mmccall10/el-transition
 
+let transitioning = new Map()
+
 // Accepts the intended state of the element
 export async function toggleWithState(element, state, transitionName = null) {
   if (state) {
@@ -31,32 +33,65 @@ export async function toggle(element, transitionName = null) {
   }
 }
 
+// transition('enter', element, null)
+// transition('leave', element, null)
+// transition('enter', element, 'dropdown')
+//
+// When transitioning, adds itself to `transitions` with direction
+// If starting a new transition, looks at transitions and removes
 async function transition(direction, element, animation) {
-  const dataset = element.dataset
-  const animationClass = animation ? `${animation}-${direction}` : direction
-  let transition = `transition${direction.charAt(0).toUpperCase() + direction.slice(1)}`
-  const genesis = dataset[transition] ? dataset[transition].split(" ") : [animationClass]
-  const start = dataset[`${transition}From`] ? dataset[`${transition}From`].split(" ") : [`${animationClass}-from`]
-  const end = dataset[`${transition}To`] ? dataset[`${transition}To`].split(" ") : [`${animationClass}-to`]
+  const classes = transitionClasses(direction, element, animation)
 
-  // if there's any overlap between the current set of classes and genesis/start/end,
+  // Cleanup interrupted transition
+  if (transitioning.has(element)) {
+    const previousDirection = transitioning.get(element)
+    const previousClasses = transitionClasses(previousDirection, element, animation)
+    console.log("INTERRUPTED by", direction, "Removing", previousClasses)
+    removeClasses(element, previousClasses.transition + previousClasses.start + previousClasses.end)
+  }
+
+  // if there's any overlap between the current set of classes and initial/start/end,
   // we should remove them before we start and add them back at the end
   const stash = []
   const current = new Set(element.classList.values())
-  genesis.forEach(cls => current.has(cls) && stash.push(cls))
-  start.forEach(cls => current.has(cls) && stash.push(cls))
-  end.forEach(cls => current.has(cls) && stash.push(cls))
+  classes.transition.forEach(cls => current.has(cls) && stash.push(cls))
+  classes.start.forEach(cls => current.has(cls) && stash.push(cls))
+  classes.end.forEach(cls => current.has(cls) && stash.push(cls))
 
+  transitioning.set(element, direction)
+
+  // Prepare transition
   removeClasses(element, stash)
-  addClasses(element, genesis)
-  addClasses(element, start)
+  addClasses(element, classes.transition)
+  addClasses(element, classes.start)
+
   await nextFrame()
-  removeClasses(element, start)
-  addClasses(element, end);
+
+  // Start transition
+  removeClasses(element, classes.start)
+  addClasses(element, classes.end)
+
   await afterTransition(element)
-  removeClasses(element, end)
-  removeClasses(element, genesis)
+
+  // Cleanup transition
+  removeClasses(element, classes.end)
+  removeClasses(element, classes.transition)
   addClasses(element, stash)
+
+  // Remove transitioning state
+  transitioning.delete(element)
+}
+
+function transitionClasses(direction, element, animation) {
+  const dataset = element.dataset
+  const animationClass = animation ? `${animation}-${direction}` : direction // 'dropdown-enter' or 'enter'
+  let transition = `transition${direction.charAt(0).toUpperCase() + direction.slice(1)}` // 'transitionEnter'
+
+  return {
+    transition: dataset[transition] ? dataset[transition].split(" ") : [animationClass], // Lookup dataset.transitionEnter classes or use ['dropdown-enter'] or ['enter']
+    start: dataset[`${transition}From`] ? dataset[`${transition}From`].split(" ") : [`${animationClass}-from`], // Lookup dataset.transitionEnterFrom classes
+    end: dataset[`${transition}To`] ? dataset[`${transition}To`].split(" ") : [`${animationClass}-to`] // Lookup dataset.transitionEnterTo classes
+  }
 }
 
 function addClasses(element, classes) {
@@ -71,11 +106,10 @@ function nextFrame() {
   return new Promise(resolve => {
     requestAnimationFrame(() => {
       requestAnimationFrame(resolve)
-    });
-  });
+    })
+  })
 }
 
 function afterTransition(element) {
   return Promise.all(element.getAnimations().map(animation => animation.finished))
 }
-
