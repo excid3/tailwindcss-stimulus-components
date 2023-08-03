c238897f2789372b0a0a5efd6ed10227827c02c5
Chris Oliver <excid3@gmail.com>
2023-08-02 21:51:46 -0500

    Add support for keyboard navigation of dropdown menus

--- src/dropdown.js
+++ src/dropdown.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 import { toggleWithState } from "./transition"
 
 export default class extends Controller {
-  static targets = ['menu', 'button']
+  static targets = ['menu', 'button', 'menuItem']
   static values = { open: Boolean, default: false }
 
   connect() {
@@ -21,6 +21,10 @@ export default class extends Controller {
 
   openValueChanged() {
     toggleWithState(this.menuTarget, this.openValue)
+
+    if (this.openValue === true && this.hasMenuItemTarget) {
+      this.menuItemTargets[0].focus()
+    }
   }
 
   show() {
@@ -35,5 +39,19 @@ export default class extends Controller {
 
   toggle() {
     this.openValue = !this.openValue
+  }
+
+  nextItem() {
+    const nextIndex = Math.min(this.currentItemIndex + 1, this.menuItemTargets.length - 1)
+    this.menuItemTargets[nextIndex].focus()
+  }
+
+  previousItem() {
+    const previousIndex = Math.max(this.currentItemIndex - 1, 0)
+    this.menuItemTargets[previousIndex].focus()
+  }
+
+  get currentItemIndex() {
+    return this.menuItemTargets.indexOf(document.activeElement)
   }
 }
