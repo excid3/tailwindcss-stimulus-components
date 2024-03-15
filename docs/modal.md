8117bdd153b469074a6bd8e9171dbf38a7aa75c3
Chris Oliver <excid3@gmail.com>
2024-03-15 13:57:14 -0500

    Use <dialog> element for modals

--- docs/modal.md
+++ docs/modal.md
@@ -10,39 +10,13 @@ application.register('modal', Modal)
 ```
 
 ```html
-<div data-controller="modal" data-action="keydown.esc->modal#close" tabindex="-1" class="relative z-10">
-  <button type="button" data-action="click->modal#open" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Open Modal</button>
-
-  <!-- Modal Background -->
-  <div class="hidden fixed inset-0 bg-black bg-opacity-80 overflow-y-auto flex items-center justify-center"
-        data-modal-target="background"
-        data-action="click->modal#closeBackground"
-        data-transition-enter="transition-all ease-in-out duration-300"
-        data-transition-enter-from="bg-opacity-0"
-        data-transition-enter-to="bg-opacity-80"
-        data-transition-leave="transition-all ease-in-out duration-300"
-        data-transition-leave-from="bg-opacity-80"
-        data-transition-leave-to="bg-opacity-0">
-
-    <!-- Modal Container -->
-    <div data-modal-target="container" class="max-h-screen w-full max-w-lg relative">
-      <!-- Modal Card -->
-      <div class="m-1 bg-white rounded shadow">
-        <div class="p-8">
-          <h2 class="text-xl mb-4">Large Modal Content</h2>
-          <p class="mb-4">This is an example modal dialog box.</p>
-
-          <div class="flex justify-end items-center flex-wrap mt-6">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-action="click->modal#close:prevent">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div data-controller="dialog">
+  <dialog data-dialog-target="dialog" class="p-8 rounded-lg backdrop:bg-black/80">
+    <p>This modal dialog has a groovy backdrop!</p>
+    <button autofocus data-action="dialog#close" class="px-2.5 py-1 bg-blue-500 text-white text-sm rounded">Close</button>
+  </dialog>
+  <button data-action="dialog#showModal" class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-2.5 rounded">Open modal</button>
 </div>
 ```
 
-`data-modal-restore-scroll-value` may be set to `false` to disable
-restoring scroll position.
-
-`data-modal-open-value` may be set to `true` to open modal on page load.
+`data-modal-open-value` may be set to `true` to open modal on connect.
--- src/modal.js
+++ src/modal.js
@@ -2,90 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 import { enter, leave } from "./transition"
 
 export default class extends Controller {
-  static targets = ['container', 'background']
+  static targets = ["dialog"]
   static values = {
-    open: { type: Boolean, default: false },
-    restoreScroll: { type: Boolean, default: true }
+    open: Boolean
   }
 
   connect() {
-    document.addEventListener("turbo:before-cache", this.beforeCache.bind(this))
+    if (this.openValue) this.showModal()
   }
 
-  disconnect() {
-    document.removeEventListener("turbo:before-cache", this.beforeCache.bind(this))
+  showModal() {
+    this.dialogTarget.showModal()
   }
 
-  open() {
-    this.openValue = true
+  show() {
+    this.dialogTarget.show()
   }
 
   close() {
-    this.openValue = false
-  }
-
-  closeBackground(event) {
-    if (event.target === this.backgroundTarget) this.close()
-  }
-
-  async openValueChanged() {
-    if (this.openValue) {
-      this.containerTarget.focus()
-      this.lockScroll()
-      enter(this.backgroundTarget)
-      enter(this.containerTarget)
-    } else {
-      leave(this.containerTarget)
-      await leave(this.backgroundTarget)
-      this.unlockScroll()
-    }
-  }
-
-  lockScroll() {
-    // Save the scroll position before we hide the scrollbar
-    if (this.restoreScrollValue) {
-      this.saveScrollPosition()
-      // Add negative top position in order for body to stay in place
-      document.body.style.top = `-${this.scrollPosition}px`
-    }
-
-    // Add right padding to the body so the page doesn't shift
-    // when we disable scrolling
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-    document.body.style.paddingRight = `${scrollbarWidth}px`
-
-    // Add classes to body to fix its position
-    document.body.classList.add('fixed', 'inset-x-0', 'overflow-hidden')
-  }
-
-  unlockScroll() {
-    // Remove tweaks for scrollbar
-    document.body.style.paddingRight = null;
-
-    // Remove classes from body to unfix position
-    document.body.classList.remove('fixed', 'inset-x-0', 'overflow-hidden')
-
-    // Restore the scroll position of the body before it got locked
-    if (this.restoreScrollValue) {
-      this.restoreScrollPosition()
-
-      // Remove the negative top inline style from body
-      document.body.style.top = null
-    }
-  }
-
-  saveScrollPosition() {
-    this.scrollPosition = window.pageYOffset || document.body.scrollTop
-  }
-
-  restoreScrollPosition() {
-    if (this.scrollPosition === undefined) return
-    document.documentElement.scrollTop = this.scrollPosition
-  }
-
-  beforeCache() {
-    this.close()
-    this.backgroundTarget.classList.add("hidden")
-    this.containerTarget.classList.add("hidden")
+    this.dialogTarget.close()
   }
 }
