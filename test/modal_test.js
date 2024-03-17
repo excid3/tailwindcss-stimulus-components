f254052c493e1f75fed4d603206d26bc15fc6692
Paul Kim <pkim@hey.com>
2024-03-16 21:28:28 -0400

    Actually test modal click

---
+++ test/modal_test.js
@@ -0,0 +1,24 @@
+import { fixture, expect, nextFrame } from '@open-wc/testing'
+import { fetchFixture } from './test_helpers'
+
+import { Application } from '@hotwired/stimulus'
+import Modal from '../src/modal'
+
+describe('ModalController', () => {
+  describe('#default', () => {
+    beforeEach(async () => {
+      const html = await fetchFixture('modal.html')
+      await fixture(html)
+      const application = Application.start()
+      application.register('modal', Modal)
+    })
+
+    it('adds open attribute when open button is clicked', async () => {
+      const dialog = document.querySelector("dialog")
+      const openModalButton = document.querySelector("[data-action='modal#open']")
+      openModalButton.click()
+
+      expect(dialog.hasAttribute("open")).to.equal(true)
+    })
+  })
+})
