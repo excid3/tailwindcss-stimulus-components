0486b3cc6999311d68278c9b95591e5f1573406d
Chris Oliver <excid3@gmail.com>
2024-03-21 16:50:46 -0500

    Update slideover tests

--- test/slideover_test.js
+++ test/slideover_test.js
@@ -14,21 +14,11 @@ describe('SlideoverController', () => {
       application.register('slideover', Slideover)
     })
 
-    it('applies visible class to target ', () => {
-      const mobileSidebar = document.querySelector('#sidebar-mobile')
-      const invisibleClass = document.querySelector('[data-controller="slideover"]').dataset
-        .slideoverInvisibleClass
-      const visibleClass = document.querySelector('[data-controller="slideover"]').dataset
-        .slideoverVisibleClass
-
-      const overLayTarget = mobileSidebar.querySelector('[data-slideover-target="overlay"]')
-      const actionBtn = document.querySelector(
-        '[data-action="click->slideover#toggle click@window->slideover#hide"]',
-      )
-
-      expect(overLayTarget.className.includes('hidden')).to.equal(true)
-      actionBtn.click()
-      expect(overLayTarget.className.includes(visibleClass)).to.equal(false)
+    it('opens dialog', () => {
+      const dialog = document.querySelector("dialog")
+      expect(dialog.hasAttribute("open")).to.equal(false)
+      document.querySelector("[data-action='slideover#open']").click()
+      expect(dialog.hasAttribute("open")).to.equal(true)
     })
   })
 })
