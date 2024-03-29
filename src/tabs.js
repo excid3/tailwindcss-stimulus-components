912a4f7a4c612c676f6b1848d1e12b22c226e240
dbil25 <dbil25@hotmail.com>
2024-03-29 16:51:12 -0400

    history.replaceState is not compatible with turbo

--- src/tabs.js
+++ src/tabs.js
@@ -71,7 +71,11 @@ export default class extends Controller {
       } else {
         const currentUrl = window.location.href // Get the current URL
         const newUrl = currentUrl.split('#')[0] + '#' + new_tab_id // Create a new URL with the updated ID
-        history.replaceState({}, document.title, newUrl) // Use history.replaceState to change the URL without scrolling
+        if(typeof Turbo !== 'undefined') {
+          Turbo.navigator.history.replace(new URL(newUrl))
+        } else {
+          history.replaceState({}, document.title, newUrl) // Use history.replaceState to change the URL without scrolling
+        }
       }
     }
   }
